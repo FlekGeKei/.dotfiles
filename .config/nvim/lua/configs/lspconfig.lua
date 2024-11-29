5786ea61a7fdf7027262e7ec0c0b406ca5bea15d
FlekGeKei <FlekGeKei@outlook.com>
2024-11-29 17:04:44 +0100

    window rules

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "bashls", "texlab" }
+local servers = { "html", "lemminx", "cssls", "bashls", "texlab" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
